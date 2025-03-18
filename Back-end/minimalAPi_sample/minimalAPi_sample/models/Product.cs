c915e5971b8320ff6804954fd039f52c3e627775
Mohamed Elsemary <110071773+Mohamed-Elsemary@users.noreply.github.com>
2025-03-18 21:43:27 +0200

    Update Product.cs

--- Back-end/minimalAPi_sample/minimalAPi_sample/models/Product.cs
+++ Back-end/minimalAPi_sample/minimalAPi_sample/models/Product.cs
@@ -5,9 +5,10 @@ namespace minimalAPi_sample.models
     public class Product
     {
         [Key]
+        // Id of the product and name of it
         public int Id { get; set; }
         public string Name { get; set; }
-
+        
         public string url { get; set; }
         public float price { get; set; }
 
